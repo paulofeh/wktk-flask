00626a3b18a408128120c564234cfdb7772afaed
Paulo Fehlauer <paulofeh@gmail.com>
2024-04-02 21:27:11 -0300

    Atualizações gerais

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# VARIÁVEIS DE AMBIENTE
+load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
+uri = os.getenv("MONGODB_URI")
+
+# Conectar ao MongoDB
+db = MongoClient(uri, ssl=True, tlsAllowInvalidCertificates=True).mjd_fehlauer
+collection = db['cvm']
 
 app = Flask(__name__)
 
@@ -6,5 +17,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/busca_empresas')
+def busca_empresas():
+    query = request.args.get('q')  # Obtém a query de busca da URL
+    if query:
+        # Busca no MongoDB usando a query; ajuste este comando conforme necessário
+        search_results = collection.find({"Nome_Companhia": {"$regex": query, "$options": "i"}})
+        suggestions = [result['Nome_Companhia'] for result in search_results]
+        return jsonify(suggestions)
+    return jsonify([])
+
+if __name__ == '__main__':
     app.run(debug=True)
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     // Inclua outros diretórios/arquivos onde você usa as classes Tailwind, se necessário
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    fontFamily: {
+      sans: ['Manrope', 'sans-serif'],
+      serif: ['Playfair Display', 'serif'],
+    },
   },
-  plugins: [require('daisyui')],
+  plugins: [require("@tailwindcss/typography"), require('tailwindcss-hero-patterns'), require("daisyui")],
+  daisyui: {
+    themes: ["forest"],
+  },
 }
