ce73f74dea52aa6459e97425fddf781a0c792953
Paulo Fehlauer <paulofeh@gmail.com>
2024-04-03 01:14:59 -0300

    Novos ajustes

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-# VARIÁVEIS DE AMBIENTE
-load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()  
 uri = os.getenv("MONGODB_URI")
 
 # Conectar ao MongoDB
@@ -17,15 +17,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/busca_empresas')
+@app.route('/busca')
 def busca_empresas():
-    query = request.args.get('q')  # Obtém a query de busca da URL
-    if query:
-        # Busca no MongoDB usando a query; ajuste este comando conforme necessário
-        search_results = collection.find({"Nome_Companhia": {"$regex": query, "$options": "i"}})
-        suggestions = [result['Nome_Companhia'] for result in search_results]
+    query = request.args.get('q')
+    if query and len(query) >= 3:
+        regex_query = {"$regex": query, "$options": "i"}  # Define a regex query
+        search_results = collection.find({
+            "$or": [
+                {"Nome_Companhia": regex_query},
+                {"AtividadesEmissorControladas.lista_atividades": regex_query}
+            ]
+        })
+        suggestions = [{"name": result['Nome_Companhia'], "slug": result.get('Codigo_CVM', '')} for result in search_results]
         return jsonify(suggestions)
     return jsonify([])
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
